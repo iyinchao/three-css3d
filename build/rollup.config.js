95e127db252eb1335684701a6c5131f823531efd
charlieyin <charlieyin@tencent.com>
2021-08-06 12:24:20 +0800

    chore: Add build scripts

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,4 +1,19 @@
+const progress = require('rollup-plugin-progress');
+const typescript = require('rollup-plugin-typescript2');
+const autoExternal = require('rollup-plugin-auto-external');
+
 module.exports = {
-  input: {},
-  output: {},
+  input: {
+    index: 'src/index.ts',
+  },
+  output: [
+    {
+      dir: 'lib',
+      entryFileNames: '[name].esm.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    {dir: 'lib', entryFileNames: '[name].js', format: 'cjs', sourcemap: true},
+  ],
+  plugins: [autoExternal(), typescript(), progress()],
 };
